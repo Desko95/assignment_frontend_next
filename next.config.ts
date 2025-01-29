70c3a2e2342f71132e50c7917d36099e141f5c06
Raziel <simonedescode@gmail.com>
2025-01-29 21:28:22 +0000

    first commit

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "deserving-harmony-9f5ca04daf.media.strapiapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      }
+    ],
+  },
+};
+
+export default nextConfig;
+
